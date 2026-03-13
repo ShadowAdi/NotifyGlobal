4e0d1f9e1ee7dd614037c00750725177785caba5
ShadowAdi <shadowshukla76@gmail.com>
2026-03-14 03:22:51 +0530

    added get all keys

--- actions/api-key.action.ts
+++ actions/api-key.action.ts
@@ -3,6 +3,7 @@ import { ActionResponse, ApiKey, CreateApiKeyDto } from "@/types";
 import { getProjectById } from "./project.action";
 import { db } from "@/db/db";
 import { apiKeys } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export const createApiKey = async (
     payload: CreateApiKeyDto,
@@ -69,6 +70,48 @@ export const createApiKey = async (
         return {
             success: false,
             error: "Failed to create contact. Please try again"
+        };
+    }
+};
+
+
+export const GetAllKeys = async (
+    token: string
+): Promise<ActionResponse<ApiKey[]>> => {
+    try {
+        if (!token) {
+            return {
+                success: false,
+                error: "Authentication required"
+            };
+        }
+
+        const authResult = await getUserIdFromToken(token);
+        if (!authResult.success) {
+            return {
+                success: false,
+                error: authResult.error
+            };
+        }
+
+        const keys = await db.select().from(apiKeys).where(eq(apiKeys.userId, authResult.userId));
+        return {
+            success: true,
+            data: keys,
+        };
+    } catch (error) {
+        console.error("Failed to get all keys:", error);
+
+        if (error instanceof Error && error.message.includes("connection")) {
+            return {
+                success: false,
+                error: "Database connection failed. Please try again later"
+            };
+        }
+
+        return {
+            success: false,
+            error: "Failed to get all keys. Please try again"
         };
     }
 };
